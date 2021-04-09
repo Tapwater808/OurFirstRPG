b478b83bff3cc184e9b31c6daacd368a9eba7bd6
Alexander Fok <foktmalexander@gmail.com>
2021-04-09 16:20:49 -0700

    Add function for validation and middleware for authentication

---
+++ auth/middleware/authenticateToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { privateKey } = require('../../config/auth.json');
+const db = require('../../models');
+
+module.exports = (req, res, next) => {
+  // const auth = req.headers.authorization;
+  // const token = auth && auth.split(' ')[1];
+  // console.log(JSON.stringify(req.cookies));
+  const accessToken = req.cookies.access_token;
+  const token = accessToken && accessToken.split(' ')[1];
+  if (!token) res.redirect('/error');
+
+  jwt.verify(token, privateKey, async (err, user) => {
+    if (err) return res.status(401).end();
+    req.user = await db.User.findOne({ where: { username: user.username } });
+    next();
+  });
+};
