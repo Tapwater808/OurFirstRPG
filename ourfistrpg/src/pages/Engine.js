8894ee492301e2dbf37545881742bcae7565269f
Alexander Fok <foktmalexander@gmail.com>
2021-04-10 11:55:29 -0700

    Move authentication related code to authTest.js

--- ourfistrpg/src/pages/Engine.js
+++ ourfistrpg/src/pages/Engine.js
@@ -1,22 +1,8 @@
-import { useEffect, useState } from "react";
+import Sprite from "./components/Sprites/index";
 
 const Engine = () => {
-  const [files, setFiles] = useState();
-  useEffect(()=> {
-    fetch('/getSaveFiles', {
-      method: 'POST'
-    }).then(response => {
-      if (response.error) {
-        console.log("REDIRECTING...");
-        setTimeout(() => window.location.replace(__dirname + '/login'), 1000);
-      } 
-      
-      response.files ? setFiles(response.files) : setFiles({});
-    });
-
-  },[])
   return (
-    <div></div>
+    <Sprite></Sprite>
   );
 }
 
