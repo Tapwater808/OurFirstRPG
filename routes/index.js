6a306931c36190cc4c882995f35fd768c17ca000
Alexander Fok <foktmalexander@gmail.com>
2021-04-10 11:43:46 -0700

    Add POST getSaveFiles

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 const loginRoute = require('./login');
 const path = require("path");
+const authenticateToken = require('../auth/middleware/authenticateToken');
 
 router.use('/login', loginRoute);
 
-router.use((req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
+router.post('/getSaveFiles', authenticateToken, (req, res)=> {
+  res.status(200).end();
+})
+
+router.use((req, res) => res.sendFile(path.join(__dirname, "../ourfistrpg/build/index.html")));
 
 module.exports = router;
   
