65a02f3a7651a42b8cd69620f14aa74ae7bcf2d1
Alexander Fok <foktmalexander@gmail.com>
2021-04-12 22:28:36 -0700

    Add configuration for direction and movement

---
+++ ourfirstrpg/src/components/Actor/vars/directions.js
@@ -0,0 +1,6 @@
+export const DOWN = 0;
+export const LEFT = 1;
+export const RIGHT = 2;
+export const UP = 3;
+const directions = {DOWN, LEFT, RIGHT, UP};
+export default directions;
---
+++ ourfirstrpg/src/components/Actor/vars/movements.js
@@ -0,0 +1,10 @@
+export const stepSize = 16;
+
+const movements = {
+  DOWN: { x: 0, y: 1},
+  LEFT: { x: -1, y: 0},
+  RIGHT: { x: 1, y: 0},
+  UP: { x: 0, y: -1 },
+}
+
+export default movements;
