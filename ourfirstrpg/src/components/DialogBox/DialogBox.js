babea6f2ae6dc562bc30208d168914755d924197
Alexander Fok <foktmalexander@gmail.com>
2021-04-17 14:56:48 -0700

    Remove name, get name from prop

--- ourfirstrpg/src/components/DialogBox/DialogBox.js
+++ ourfirstrpg/src/components/DialogBox/DialogBox.js
@@ -1,8 +1,6 @@
 import "../DialogBox/dialogBox.css"
 
-const name = "Buckaroo";
-
-const DialogBox = ({message, isVisible}) => {
+const DialogBox = ({isVisible, name, message}) => {
     return (
         <div className={ isVisible ? 'DialogWindow2' : 'DialogWindow'}>
             <div className="DialogTitle">{name}</div>
