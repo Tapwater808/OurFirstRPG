4ef97ad88d5fbc7e26142f63fd88dd16851cb083
Alexander Fok <foktmalexander@gmail.com>
2021-04-17 08:58:42 -0700

    call update in caller instead of directly setting player position

--- ourfirstrpg/src/components/Actor/actions/useWalk.js
+++ ourfirstrpg/src/components/Actor/actions/useWalk.js
@@ -1,41 +1,29 @@
 import { useState } from 'react';
 import directions from '../vars/directions';
-import movements, {stepSize} from '../vars/movements';
-import mapSize, {UNIT as unit} from '../../Map/vars/mapSize';
+import movements from '../vars/movements';
 
-// Parameters:
-//   map:         the grid representation of the map
-//   setPosition: function to update position of character
-//   charSize:    size of character (in grid)
-const useWalk = (map, setPosition, charSize) => {
+const useWalk = (update, isObstacle) => {
   const [gesture, setGesture] = useState(0);
   const [dir, setDir] = useState(0);
   
   const move = (dir) => {
-    setPosition(prev => {
+    update(prev => {
       const next = {
         x: prev.x + movements[dir].x,
         y: prev.y + movements[dir].y
       }
-      const offset = {
-        x: mapSize.x - 1 - charSize.width,
-        y: mapSize.y - 1 - charSize.height
-      }
-      
-      const isBoundary = next.x < 0 || next.x > offset.x || next.y < 0 || next.y > offset.y;
-      if (isBoundary) return prev;
-      
-      const isWall = map[next.y][next.x] === 1;
-      return isWall ? prev: next
+      const pos = isObstacle(next) ? prev : next;
+      return [dir, pos];
     });
   }
 
   const walk = (dir) => {
     setGesture((prev) => prev < 2 ? prev + 1 : 0);
     setDir((prev) => {
-      if (prev === directions[dir]) move(dir);
+      prev === directions[dir] ? move(dir) : update(pos => [dir, pos]);
       return directions[dir]
     });
+    
   }
   
   return {
