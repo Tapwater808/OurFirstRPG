db1e3e72eaefa0681eee415cfedd9a79d14a78f5
Alexander Fok <foktmalexander@gmail.com>
2021-04-17 14:03:55 -0700

    Remove props and button

--- ourfirstrpg/src/components/DialogBox/DialogBox.js
+++ ourfirstrpg/src/components/DialogBox/DialogBox.js
@@ -2,43 +2,15 @@ import "../DialogBox/dialogBox.css"
 
 const name = "Buckaroo";
 
-const messages = [
-    "This is a test",
-    "Dammit",
-    "I've been banned from Dave & Busters"
-];
-
-const DialogBox = ({
-    isVisible,
-    setIsVisible,
-    currentMessage,
-    setCurrentMessage
-}) => {
-    const handleClick = () => {
-        if (currentMessage < messages.length - 1) {
-            setCurrentMessage(currentMessage + 1);
-        } else {
-            setIsVisible(false);
-            setCurrentMessage(0)
-        }
-    };
-
-    const onClick = () => setIsVisible(true)
-
+const DialogBox = ({message, isVisible}) => {
     return (
-        <>
-        <button className="DialogButton" onClick={onClick}>Show Dialog</button>
-
-        <div className="container">
-        </div>
         <div className={ isVisible ? 'DialogWindow2' : 'DialogWindow'}>
             <div className="DialogTitle">{name}</div>
-            <div className="DialogText">{messages[currentMessage]}</div>
-            <div onClick={handleClick} className="DialogFooter">
+            <div className="DialogText">{message}</div>
+            <div className="DialogFooter">
                 --{`>`}
             </div>
         </div>
-        </>
     );
 };
 
