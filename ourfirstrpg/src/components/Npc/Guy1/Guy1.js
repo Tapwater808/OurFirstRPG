ba5792d25d58d90ccd4cffe6e25232e148dd7bfc
Alexander Fok <foktmalexander@gmail.com>
2021-04-17 08:48:49 -0700

    Lift useWalk hook to parent component

--- ourfirstrpg/src/components/Npc/Guy1/Guy1.js
+++ ourfirstrpg/src/components/Npc/Guy1/Guy1.js
@@ -1,27 +1,15 @@
-import { useEffect } from 'react';
 import Actor from '../../Actor';
-import useWalk from '../../Actor/actions/useWalk';
 import directions from '../../Actor/vars/directions';
-import {UNIT as unit} from '../../Map/vars/mapSize';
 
-const Guy1 = ({initPos, map, playerLocation}) => {
-    const charSize = { width: 32, height: 32 };
-    const {position, dir} = useWalk(initPos, map, {
-        width: parseInt(charSize.width/unit),
-        height: parseInt(charSize.height/unit)
-      }
-    );
-    useEffect(() => {
-      
-    })
-
+const Guy1 = ({position}) => {
+    const charSize = { width: 4, height: 4 };
     return (
       <Actor
         image='url(./Assets/BadGuy1.png)'
         position={position}
         size={charSize}
         step={0}
-        dir={dir}
+        dir={directions.DOWN}
       />
     );
   }
--- ourfirstrpg/src/components/Player/index.js
+++ ourfirstrpg/src/components/Player/index.js
@@ -1,26 +1,11 @@
 
 import Actor from '../Actor';
-import {UNIT as unit} from '../Map/vars/mapSize';
 
 const Player = ({position, size, gesture, dir}) => {
-  const info = {
-    image: 'url(./Assets/Player.png)',
-    position: {
-      x: position.x * unit,
-      y: position.y * unit
-    },
-    size,
-    gesture,
-    dir
-  };
-  console.log(info);
   return (
     <Actor
       image='url(./Assets/Player.png)'
-      position={{
-        x: position.x * unit,
-        y: position.y * unit
-      }}
+      position={position}
       size={size}
       step={gesture}
       dir={dir}
