55b3ab0a9366635a7450747369aa677ca4f2b9a9
Alexander Fok <foktmalexander@gmail.com>
2021-04-17 14:04:37 -0700

    Add dialog in spawn

--- ourfirstrpg/src/components/Npc/vars/spawn.js
+++ ourfirstrpg/src/components/Npc/vars/spawn.js
@@ -3,7 +3,12 @@ import sprite from '../../Sprite/vars/config';
 const village = {
   '12,16': {
     location: {x: 12, y: 16},
-    sprite: sprite.BadGuy1
+    sprite: sprite.BadGuy1,
+    dialog: [
+      "This is a test",
+      "Dammit",
+      "I've been banned from Dave & Busters"
+    ]
   }
 }
 
