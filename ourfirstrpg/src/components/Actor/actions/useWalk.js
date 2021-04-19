6dde31ec37eb3db01e9c6bce0714baf891cc6896
Alexander Fok <foktmalexander@gmail.com>
2021-04-19 02:13:32 -0700

    Rename 'pos' to 'loc'. Remove reference 'direction'

--- ourfirstrpg/src/components/Actor/actions/useWalk.js
+++ ourfirstrpg/src/components/Actor/actions/useWalk.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import directions from '../vars/directions';
 import movements from '../vars/movements';
 
 const useWalk = (update, isObstacle) => {
@@ -12,25 +11,17 @@ const useWalk = (update, isObstacle) => {
         x: prev.x + movements[dir].x,
         y: prev.y + movements[dir].y
       }
-      const pos = isObstacle(next) ? prev : next;
-      return [dir, pos];
+      const loc = isObstacle(next) ? prev : next;
+      return [dir, loc];
     });
   }
-
   const walk = (dir) => {
     setGesture((prev) => prev < 2 ? prev + 1 : 0);
     setDir((prev) => {
-      prev === directions[dir] ? move(dir) : update(pos => [dir, pos]);
-      return directions[dir]
+      prev === dir ? move(dir) : update(loc => [dir, loc]);
+      return dir;
     });
-    
   }
-  
-  return {
-    gesture,
-    dir,
-    walk
-  }
+  return [gesture, dir, walk]
 }
-
 export default useWalk;
