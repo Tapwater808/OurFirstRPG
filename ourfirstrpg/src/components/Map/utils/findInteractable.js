72a1c9af2f478c97f521a22ea2713d065406512e
Alexander Fok <foktmalexander@gmail.com>
2021-04-19 02:20:07 -0700

    Add function to find interactable block in surroundings

---
+++ ourfirstrpg/src/components/Map/utils/findInteractable.js
@@ -0,0 +1,54 @@
+import { DOWN, LEFT, RIGHT, UP } from '../../Actor/vars/directions';
+import { DOOR, NPC, ITEM } from '../vars/tileTypes';
+import spawn from '../../Npc/vars/spawn';
+
+const findInteractable = (mapName, map, loc, dir, { width, height }) => {
+  const isOnInteractable = (type) => (type === ITEM);
+  const isFacingInteractable = (type) => (type === DOOR) || (type === NPC);
+
+  const on = [
+    map[loc.y][loc.x],
+    map[loc.y][loc.x - 1],
+    map[loc.y][loc.x + 1],
+    map[loc.y - 1][loc.x],
+    map[loc.y - 1][loc.x - 1],
+    map[loc.y - 1][loc.x + 1],
+    map[loc.y + 1][loc.x],
+    map[loc.y + 1][loc.x - 1],
+    map[loc.y + 1][loc.x + 1],
+  ]
+
+  const inSight = {
+    [DOWN]: [
+      map[loc.y + height][loc.x],
+      map[loc.y + height][loc.x - 1],
+      map[loc.y + height][loc.x + 1],
+    ],
+    [LEFT]: [
+      map[loc.y][loc.x - 1],
+      map[loc.y - 1][loc.x - 1],
+      map[loc.y + 1][loc.x - 1],
+    ],
+    [RIGHT]: [
+      map[loc.y][loc.x + width],
+      map[loc.y - 1][loc.x + width],
+      map[loc.y + 1][loc.x + width],
+    ],
+    [UP]: [
+      map[loc.y - 1][loc.x],
+      map[loc.y - 1][loc.x - 1],
+      map[loc.y - 1][loc.x + 1],
+    ]
+  }
+  
+  const onEntity = on.find(tile => isOnInteractable(tile.type));
+  const inSightEntity = inSight[dir].find(tile => isFacingInteractable(tile.type));
+
+  const interactable =  onEntity || inSightEntity;
+
+  const findInfo = (obj) => ({
+    [NPC]: spawn[mapName][`${obj.location.x},${obj.location.y}`]
+  })
+  return interactable ? findInfo(interactable) : null;
+}
+export default findInteractable;
