610c78fa8701725c1b0777770732ccb647eac232
Alexander Fok <foktmalexander@gmail.com>
2021-04-19 02:16:09 -0700

    Replace digit (0's and 1's) with tile obj for each block of map

--- ourfirstrpg/src/components/Map/utils/buildMap.js
+++ ourfirstrpg/src/components/Map/utils/buildMap.js
@@ -1,15 +1,28 @@
 import spawn from '../../Npc/vars/spawn';
-import {UNIT as unit} from '../vars/mapSize';
+import { SPACE, NPC } from '../vars/tileTypes';
+
+const createTile = ({type = SPACE, location = {x: 0, y: 0}}) => ({ type, location })
+
 const buildMap = (name, mapArray) => {
-  const modified = JSON.parse(JSON.stringify(mapArray));
+  // Build map base on 2D map array
+  const modified = mapArray.map((row, r) =>
+    row.map((col, c) => createTile({
+      type: col, // SPACE, WALL OR DOOR
+      location: {x: c, y: r}
+    }))
+  )
+  // Add in NPC's
   const npcs = spawn[name];
-  
   for (const coordinates in npcs){
     const npc = npcs[coordinates];
     const {width, height} = npc.sprite.size;
-    for (let h = 0; h < height/unit; h++) {
-      for (let w = 0; w < width/unit; w++){
-        modified[npc.location.y + h][npc.location.x + w] = 1
+    for (let h = 0; h < height; h++) {
+      for (let w = 0; w < width; w++) {
+        modified[npc.location.y + h][npc.location.x + w] = 
+          createTile({
+            type: NPC,
+            location: npc.location
+          })
       }
     }
   };
