e6cf7f169d8773525228a91e533dc924c61e001c
Alexander Fok <foktmalexander@gmail.com>
2021-04-20 15:53:51 -0700

    Add in logic for creating door tiles

--- ourfirstrpg/src/components/Map/utils/buildMap.js
+++ ourfirstrpg/src/components/Map/utils/buildMap.js
@@ -1,5 +1,6 @@
 import spawn from '../../Npc/vars/spawn';
-import { SPACE, NPC } from '../vars/tileTypes';
+import wrapPoint, {SIZE as DOORSIZE} from '../../Door/vars/wrapPoint';
+import { SPACE, NPC, DOOR } from '../vars/tileTypes';
 
 const createTile = ({type = SPACE, location = {x: 0, y: 0}}) => ({ type, location })
 
@@ -7,10 +8,25 @@ const buildMap = (name, mapArray) => {
   // Build map base on 2D map array
   const modified = mapArray.map((row, r) =>
     row.map((col, c) => createTile({
-      type: col, // SPACE, WALL OR DOOR
+      type: col, // SPACE, WALL
       location: {x: c, y: r}
     }))
   )
+  // Add in Doors
+  const doors = wrapPoint[name];
+  for (const coordinates in doors){
+    const door = doors[coordinates];
+    for (let h = 0; h < DOORSIZE.height; h++) {
+      for (let w = 0; w < DOORSIZE.width; w++) {
+        modified[door.location.y + h][door.location.x + w] = 
+          createTile({
+            type: DOOR,
+            location: door.location,
+          })
+      }
+    }
+  }
+
   // Add in NPC's
   const npcs = spawn[name];
   for (const coordinates in npcs){
@@ -25,7 +41,7 @@ const buildMap = (name, mapArray) => {
           })
       }
     }
-  };
+  }
   return modified;
 }
 export default buildMap;
