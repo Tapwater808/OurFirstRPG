cffb52d7f2a4ed349bd0aa1cd659457b99e7ceea
Alexander Fok <foktmalexander@gmail.com>
2021-04-20 15:51:31 -0700

    Add wrap point (door) in the house map

---
+++ ourfirstrpg/src/components/Door/vars/wrapPoint.js
@@ -0,0 +1,12 @@
+export const SIZE = {width: 4, height: 2}
+
+const house = {
+  '12,22': {
+    location: {x: 12, y: 22},
+    to: 'village',
+    spawnPlayerAt: {x: 12, y: 10}
+  }
+}
+
+const wrapPoint = {house}
+export default wrapPoint;
