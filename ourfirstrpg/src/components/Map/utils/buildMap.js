b95b29c6a6e3783e247b8f405100304bd64c2d98
Alexander Fok <foktmalexander@gmail.com>
2021-04-21 10:41:54 -0700

    Use 32 * 32 blocks for building map

--- ourfirstrpg/src/components/Map/utils/buildMap.js
+++ ourfirstrpg/src/components/Map/utils/buildMap.js
@@ -1,25 +1,18 @@
 import spawn from '../../Npc/vars/spawn';
 import wrapPoint, {SIZE as DOORSIZE} from '../../Door/vars/wrapPoint';
-import { SPACE, NPC, DOOR } from '../vars/tileTypes';
+import { NPC, DOOR } from '../vars/tileTypes';
 import { MAP_SIZE_WIDTH, MAP_SIZE_HEIGHT } from '../vars/mapSize';
-const createTile = ({type = SPACE, location = {x: 0, y: 0}}) => ({ type, location })
-// mapArray = 32 px version of map
+const createTile = ({type, location}) => ({ type, location })
+
 const buildMap = (name, mapArray) => {
-  // Build map base on 2D map array
-  let modified = new Array(MAP_SIZE_HEIGHT).fill(new Array(MAP_SIZE_WIDTH));
-  console.log('mapArray: ', mapArray);
-  mapArray.forEach((row, r) =>
-    row.forEach((col, c) => {
-      // for each block
-      for (let h = 0; h < 4; h++) {
-        for (let w = 0; w < 4; w++) {
-          modified[r * 4 + h][c * 4 + w] = createTile({
-            type: col, // SPACE, WALL OR DOOR
-            location: {x: c, y: r}
-          })
-        }
-      }
-    })
+  let modified = new Array(MAP_SIZE_HEIGHT).fill(new Array(MAP_SIZE_WIDTH).fill(null));
+  modified = modified.map((row,r) => 
+    row.map((col,c) => 
+      createTile({
+        type: mapArray[Math.floor(r * .25)][Math.floor(c * .25)],
+        location: { x: c, y: r }
+      })
+    )
   )
   // Add in Doors
   const doors = wrapPoint[name];
