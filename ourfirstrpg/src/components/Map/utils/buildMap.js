4c5bd965d63cafb448e9ea4a0df4c92bc0ae1f8f
Alexander Fok <foktmalexander@gmail.com>
2021-04-20 20:15:47 -0700

    Add logic to covert 32 px block 2D array to 8px tile map

--- ourfirstrpg/src/components/Map/utils/buildMap.js
+++ ourfirstrpg/src/components/Map/utils/buildMap.js
@@ -1,15 +1,24 @@
 import spawn from '../../Npc/vars/spawn';
 import { SPACE, NPC } from '../vars/tileTypes';
-
+import { MAP_SIZE_WIDTH, MAP_SIZE_HEIGHT } from '../vars/mapSize';
 const createTile = ({type = SPACE, location = {x: 0, y: 0}}) => ({ type, location })
-
+// mapArray = 32 px version of map
 const buildMap = (name, mapArray) => {
   // Build map base on 2D map array
-  const modified = mapArray.map((row, r) =>
-    row.map((col, c) => createTile({
-      type: col, // SPACE, WALL OR DOOR
-      location: {x: c, y: r}
-    }))
+  let modified = new Array(MAP_SIZE_HEIGHT).fill(new Array(MAP_SIZE_WIDTH));
+  console.log('mapArray: ', mapArray);
+  mapArray.forEach((row, r) =>
+    row.forEach((col, c) => {
+      // for each block
+      for (let h = 0; h < 4; h++) {
+        for (let w = 0; w < 4; w++) {
+          modified[r * 4 + h][c * 4 + w] = createTile({
+            type: col, // SPACE, WALL OR DOOR
+            location: {x: c, y: r}
+          })
+        }
+      }
+    })
   )
   // Add in NPC's
   const npcs = spawn[name];
