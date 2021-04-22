f35d5fef86a6f7e2cf9e997409c45d6d9644c713
EJostad <eric.jostad@outlook.com>
2021-04-21 23:51:05 -0700

    Added dungeon.js and corresponding impassable tile code.

--- ourfirstrpg/src/components/Map/vars/maps/index.js
+++ ourfirstrpg/src/components/Map/vars/maps/index.js
@@ -1,7 +1,8 @@
 import worldMap from './worldMap';
 import village from './village';
+import dungeon from './dungeon';
 import store from './store';
 
-const maps = {worldMap, village, store};
+const maps = {worldMap, village, dungeon, store};
 
 export default maps;
