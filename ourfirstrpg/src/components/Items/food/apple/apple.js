86fc8ee0f183f88952e6e7a6eaef5eeed28c0f3a
EJostad <eric.jostad@outlook.com>
2021-04-22 22:41:47 -0700

    Small change to apple.js.

--- ourfirstrpg/src/components/Items/food/apple/apple.js
+++ ourfirstrpg/src/components/Items/food/apple/apple.js
@@ -1,21 +1,22 @@
-import Actor from '../Actor';
+import Actor from '../../Actor';
 import directions from '../../Actor/vars/directions';
 
-const Apple = ({position, size, gesture, dir}) => {
-  return (
-    <Actor
-      image='url(./Assets/Apple.jpg)'
-      position={position}
-      size={size}
-      step={gesture}
-      dir={directions.DOWN}
-    />
-  );
-}
-export default Apple;
+const Apple= ({position}) => {
+    const charSize = { width: 4, height: 4 };
+    return (
+      <Actor
+        image='url(./Assets/Apple.jpeg)'
+        position={position}
+        size={charSize}
+        step={0}
+        dir={directions.DOWN}
+      />
+    );
+  }
+  export default Apple;
 
-export const NAME = 'Apple'
+  export const NAME = 'Apple'
 
-export const MESSAGES = [
-  'Yum, an apple!'
-];
+  export const MESSAGES = [
+    'Yum, an apple!'
+  ];
