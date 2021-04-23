27539cf05527f8d3fa71c39f1cc201c1f6caf5bf
EJostad <eric.jostad@outlook.com>
2021-04-22 22:42:51 -0700

    Small change to banana.js.

--- ourfirstrpg/src/components/Items/food/banana/banana.js
+++ ourfirstrpg/src/components/Items/food/banana/banana.js
@@ -1,21 +1,22 @@
-import Actor from '../Actor';
+import Actor from '../../Actor';
 import directions from '../../Actor/vars/directions';
 
-const Banana= ({position, size, gesture, dir}) => {
-  return (
-    <Actor
-      image='url(./Assets/Banana.png)'
-      position={position}
-      size={size}
-      step={gesture}
-      dir={directions.DOWN}
-    />
-  );
-}
-export default Banana;
+const Banana = ({position}) => {
+    const charSize = { width: 4, height: 4 };
+    return (
+      <Actor
+        image='url(./Assets/Banana.png)'
+        position={position}
+        size={charSize}
+        step={0}
+        dir={directions.DOWN}
+      />
+    );
+  }
+  export default Banana;
 
-export const NAME = 'Banana'
+  export const NAME = 'Banana'
 
-export const MESSAGES = [
-  'Yum, a Banana!'
-];
+  export const MESSAGES = [
+    'Yum, a banana!'
+  ];
