ad564875d0ace564d88b8ba92b59ce393740846e
EJostad <eric.jostad@outlook.com>
2021-04-22 21:20:17 -0700

    Added Apple & Banana objects to village const within spawn.js

--- ourfirstrpg/src/components/Npc/vars/spawn.js
+++ ourfirstrpg/src/components/Npc/vars/spawn.js
@@ -10,6 +10,22 @@ const village = {
       'I am your wife',
       'Make me a sandwich, please :)'
     ]
+  },
+  '18,20': {
+    location: {x: 18, y: 20},
+    name: 'Apple',
+    sprite: sprite.Apple,
+    dialog: [
+      'Yum, an apple!'
+    ]
+  },
+  '22,24': {
+    location: {x: 22, y: 24},
+    name: 'Banana',
+    sprite: sprite.Banana,
+    dialog: [
+      'Yum, a banana!'
+    ]
   }
 }
 
