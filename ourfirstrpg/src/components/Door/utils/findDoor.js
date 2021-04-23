c5897c2d166c4f7d479eb238565d8e8dc58db05c
Alexander Fok <foktmalexander@gmail.com>
2021-04-22 21:44:43 -0700

    Add corners for checking player location more accurately

--- ourfirstrpg/src/components/Door/utils/findDoor.js
+++ ourfirstrpg/src/components/Door/utils/findDoor.js
@@ -2,8 +2,11 @@ import { DOOR } from '../../Map/vars/tileTypes';
 import wrapPoint from '../vars/wrapPoint';
 
 const findDoor = (mapName, map, loc) => {
-  const tile = map[loc.y][loc.x];
-  
-  return tile.type === DOOR ? wrapPoint[mapName][`${loc.x},${loc.y}`] : null
+  const corners = [
+    map[loc.y][loc.x],
+    map[loc.y + 1][loc.x + 1]
+  ];
+  const doorTile = corners.find(tile => tile.type === DOOR);
+  return doorTile ? wrapPoint[mapName][`${doorTile.location.x},${doorTile.location.y}`] : null
 }
 export default findDoor;
