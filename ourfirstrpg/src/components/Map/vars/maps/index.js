db671487f298fada7a8d7d4b83e34b91af30ceb6
Alexander Fok <foktmalexander@gmail.com>
2021-04-22 20:36:17 -0700

    Remove village

--- ourfirstrpg/src/components/Map/vars/maps/index.js
+++ ourfirstrpg/src/components/Map/vars/maps/index.js
@@ -1,9 +1,8 @@
 import world from './worldMap';
-import village from './village';
 import dungeon from './dungeon';
 import house from './house';
 import store from './store';
 
-const maps = {world, village, dungeon, house, store};
+const maps = {world, dungeon, house, store};
 
 export default maps;
