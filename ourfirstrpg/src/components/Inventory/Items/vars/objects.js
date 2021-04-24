4a287bad3502266c4ac81d30a0250a71a61c3625
EJostad <eric.jostad@outlook.com>
2021-04-24 13:45:39 -0700

    Added vars subfolder in Inventory>Items as well as starter code.

---
+++ ourfirstrpg/src/components/Inventory/Items/vars/objects.js
@@ -0,0 +1,10 @@
+import Apple from '../../../Items/food/apple/apple';
+import Banana from '../../../Items/food/banana/banana';
+
+var worldStuff = [
+    Apple= new Item ("apple", 0),
+    Banana = new Item ("banana", 3),
+];
+
+Apple.description = "Your father's pouch, made of Spanish leather.";
+Banana.description = "A half-eaten piece of liquorice.";
