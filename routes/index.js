cce53b86bf9473e23325d1690475f8c0d634be2d
kytaylor <ky.taylor5468@gmail.com>
2021-04-25 20:59:22 -0700

    attempting to fix sign in error

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,34 @@ router.post('/getSaveFiles', authenticateToken, (req, res)=> {
   res.status(200).end();
 })
 
+// router.post('/signup', async (req, res) => {
+//   const signUpObj = await db.User.findOne({ username: req.params.username })
+//   if (!signUpObj) {
+//     const newSignUpObj = await db.User.create(req.body);
+//     console.log(req.body);
+//     res.json(newSignUpObj);
+//   }
+//     res.status(403).end();
+// });
+
+let signUpObj = '';
+
 router.post('/signup', async (req, res) => {
-  const signUpObj = await db.User.findOne({ username: req.params.username })
-  if (!signUpObj) {
-    const newSignUpObj = await db.User.create(req.body);
-    console.log(req.body);
-    res.json(newSignUpObj);
-  }
-    res.status(403).end();
+    try {
+      signUpObj = await db.User.findOne({ username: req.params.username })
+    }
+    catch(err) {
+      console.log(err);
+      res.status(401).end();
+    }
+    finally {
+      if (!signUpObj) {
+        const newSignUpObj = await db.User.create(req.body);
+        console.log(req.body);
+        res.json(newSignUpObj);
+      }
+        res.status(403).end();
+    }
 });
 
 router.use((req, res) => res.sendFile(path.join(__dirname, "../ourfistrpg/build/index.html")));
