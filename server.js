0a6e8a36ab8296f85d0e8b27835d9d8381661592
Alexander Fok <foktmalexander@gmail.com>
2021-04-29 16:28:47 -0700

    Add code for heroku production

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ourfirstrpg");
